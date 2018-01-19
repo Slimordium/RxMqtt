65e6bee2871cc79af1c8618a05f174603ad81ba4
Larry Watkins <lwatkins@colbi.com>
2018-01-18 17:46:19 -0700

    Fixed packages. Fixed broker.

---
+++ RxMqtt.Broker/Client.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Text;
+using NLog;
+using RxMqtt.Shared;
+using RxMqtt.Shared.Messages;
+
+namespace RxMqtt.Broker
+{
+    internal class Client
+    {
+        internal string ClientId { get; set; }
+        internal byte[] Buffer { get; set; } = new byte[128000];
+        internal Socket Socket { get; set; }
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        internal void IncomingMessage(Publish mqttMessage)
+        {
+            BeginSend( mqttMessage.GetBytes());
+        }
+
+        internal void Start()
+        {
+            Socket.BeginReceive(Buffer, 0, 128000, 0, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult asyncResult)
+        {
+            //var socket = (Socket)asyncResult.AsyncState;
+            var bytesIn = 0;
+
+            try
+            {
+                bytesIn = Socket.EndReceive(asyncResult);
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, e);
+                return;
+            }
+
+            if (bytesIn <= 0)
+                return;
+
+            var newBuffer = new byte[bytesIn];
+
+            Array.Copy(Buffer, 0, newBuffer, 0, bytesIn);
+
+            Buffer = new byte[128000];
+
+            var msgType = (MsgType)(byte)((newBuffer[0] & 0xf0) >> (byte)MsgOffset.Type);
+
+            _logger.Log(LogLevel.Trace, $"In <= {msgType}");
+
+            switch (msgType)
+            {
+                case MsgType.Publish:
+                    var publishMsg = new Publish(newBuffer);
+
+                    BeginSend(new PublishAck(publishMsg.PacketId).GetBytes());
+
+                    MqttBroker.MqttMessagesSubject.OnNext(publishMsg);
+                    break;
+                case MsgType.Connect:
+                    var connectMsg = new Connect(newBuffer);
+
+                    _logger.Log(LogLevel.Trace, $"Client '{connectMsg.ClientId}' connected. Sending ConnectAck");
+
+                    ClientId = connectMsg.ClientId;
+
+                    _logger = LogManager.GetLogger(ClientId);
+
+                    BeginSend(new ConnectAck().GetBytes());
+                        
+                    break;
+                case MsgType.PingRequest:
+                    _logger.Log(LogLevel.Trace, $"Sending ping response '{ClientId}'");
+                    BeginSend(new PingResponse().GetBytes());
+                    break;
+                case MsgType.Subscribe:
+                    var subscribeMsg = new Subscribe(newBuffer);
+
+                    foreach (var topic in subscribeMsg.Topics)
+                    {
+                        MqttBroker.ObservableMqttMsgs.Where(m => m.MsgType == MsgType.Publish && m.Topic.Equals(topic, StringComparison.InvariantCultureIgnoreCase)).Subscribe(IncomingMessage);
+                    }
+
+                    BeginSend(new SubscribeAck(subscribeMsg.PacketId).GetBytes());
+                    break;
+                case MsgType.PublishAck:
+                    break;
+                default:
+                    _logger.Log(LogLevel.Warn, $"Ignoring message: '{Encoding.UTF8.GetString(newBuffer)}'");
+                    break;
+            }
+
+            Socket.BeginReceive(Buffer, 0, 128000, 0, ReceiveCallback, Socket);
+        }
+
+        private void BeginSend(byte[] buffer)
+        {
+            if (!Socket.Connected)
+                return;
+
+            Socket.BeginSend(buffer, 0, buffer.Length, 0, SendCallback, null);
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var bytesSent = Socket.EndSend(ar);
+
+                _logger.Log(LogLevel.Trace, $"Sent '{bytesSent}' bytes");
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, e);
+            }
+        }
+    }
+}
--- RxMqtt.Broker/MqttBroker.cs
+++ RxMqtt.Broker/MqttBroker.cs
@@ -4,34 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Text;
 using System.Threading;
 using NLog;
 using RxMqtt.Shared;
+using RxMqtt.Shared.Messages;
 
 namespace RxMqtt.Broker
 {
     public class MqttBroker
     {
-        private readonly ManualResetEvent _acceptConnectionResetEvent = new ManualResetEvent(false);
+        private readonly AutoResetEvent _acceptConnectionResetEvent = new AutoResetEvent(false);
 
         private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
 
         private readonly IPAddress _ipAddress = IPAddress.Any;
 
+        internal static IObservable<Publish> ObservableMqttMsgs => MqttMessagesSubject.AsObservable();
+
+        internal static Subject<Publish> MqttMessagesSubject { get; } = new Subject<Publish>(); 
+
         /// <summary>
         /// Clients/sockets
         /// </summary>
-        private static readonly ConcurrentDictionary<string, ClientState> _clientStates = new ConcurrentDictionary<string, ClientState>();
+        private static readonly List<Client> _clients = new List<Client>();
 
-        /// <summary>
-        /// Subscriptions by client
-        /// </summary>
-        private static readonly Dictionary<string, List<string>> _clientSubscriptions = new Dictionary<string, List<string>>();
-
-        /// <summary>
-        /// Recursivly handle read/write of all clients
-        /// </summary>
         public MqttBroker()
         {
             _logger.Log(LogLevel.Warn, "Binding to all local addresses");
@@ -62,12 +61,9 @@ namespace RxMqtt.Broker
             {
                 try
                 {
-                    _acceptConnectionResetEvent.Reset();
-
                     listener.BeginAccept(AcceptConnectionCallback, listener);
 
-                    //Wait 30 seconds for client connection to finish
-                    _acceptConnectionResetEvent.WaitOne(TimeSpan.FromSeconds(30));
+                    _acceptConnectionResetEvent.WaitOne();
                 }
                 catch (Exception e)
                 {
@@ -75,145 +71,24 @@ namespace RxMqtt.Broker
                 }
             }
         }
-
+        
+        //TODO: Dispose clients whent they disconnect
         private void AcceptConnectionCallback(IAsyncResult asyncResult)
         {
+            _acceptConnectionResetEvent.Set();
+
             _logger.Log(LogLevel.Trace, $"Client connecting...");
 
             var listener = (Socket)asyncResult.AsyncState;
             var socket = listener.EndAccept(asyncResult);
 
-            var state = new ClientState { Socket = socket };
+            var client = new Client { Socket = socket };
 
-            socket.BeginReceive(state.Buffer, 0, 128000, 0, ReceiveCallback, state);
+            client.Start();
+
+            _clients.Add(client);
 
             _logger.Log(LogLevel.Trace, $"Client connected");
-
-            _acceptConnectionResetEvent.Set();
-        }
-
-        private static readonly object _theLock = new object();
-
-        static SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
-
-        private void ReceiveCallback(IAsyncResult asyncResult)
-        {
-            _semaphoreSlim.Wait();
-
-            try
-            {
-                var client = (ClientState)asyncResult.AsyncState;
-                var socket = client.Socket;
-                var bytesIn = 0;
-
-                try
-                {
-                    bytesIn = socket.EndReceive(asyncResult);
-                }
-                catch (Exception e)
-                {
-                    _logger.Log(LogLevel.Error, e);
-                    return;
-                }
-
-                if (bytesIn <= 0)
-                    return;
-
-                var newBuffer = new byte[bytesIn];
-
-                Array.Copy(client.Buffer, 0, newBuffer, 0, bytesIn);
-
-                client.Buffer = new byte[128000];
-
-                var msgType = (MsgType)(byte)((newBuffer[0] & 0xf0) >> (byte)MsgOffset.Type);
-
-                _logger.Log(LogLevel.Trace, $"In <= {msgType}");
-
-                switch (msgType)
-                {
-                    case MsgType.Publish:
-                        var publishMsg = new Shared.Messages.Publish(newBuffer);
-
-                        foreach (var subscriptionMapping in _clientSubscriptions.Where(subscription => subscription.Value.Contains(publishMsg.Topic)).ToList())
-                        {
-                            if (subscriptionMapping.Key.Equals(client.ClientId))
-                            {
-                                _logger.Log(LogLevel.Warn, $"'{client.ClientId}' - recursive publish, ignoring message");
-                                continue;
-                            }
-
-                            _logger.Log(LogLevel.Trace, $"'{subscriptionMapping.Key}' is subscribed to '{publishMsg.Topic}', Publishing message");
-                            BeginSend(_clientStates[subscriptionMapping.Key], new Shared.Messages.Publish(publishMsg.Topic, publishMsg.Message).GetBytes());
-                        }
-                        break;
-                    case MsgType.Connect:
-                        var connectMsg = new Shared.Messages.Connect(newBuffer);
-
-                        _logger.Log(LogLevel.Trace, $"Client '{connectMsg.ClientId}' connected. Sending ConnectAck");
-
-                        client.ClientId = connectMsg.ClientId;
-
-                        //Remove old state/socket if already exists
-                        _clientStates.TryRemove(connectMsg.ClientId, out var tempClient);
-                        _clientStates.TryAdd(connectMsg.ClientId, client);
-
-                        //Remove subscriptions if already exists
-                        _clientSubscriptions.Remove(connectMsg.ClientId);
-                        _clientSubscriptions.Add(connectMsg.ClientId, new List<string>());
-
-                        BeginSend(client, new Shared.Messages.ConnectAck().GetBytes());
-                        break;
-                    case MsgType.PingRequest:
-                        _logger.Log(LogLevel.Trace, $"Sending ping response '{client.ClientId}'");
-                        BeginSend(client, new Shared.Messages.PingResponse().GetBytes());
-                        break;
-                    case MsgType.Subscribe:
-                        var subscribeMsg = new Shared.Messages.Subscribe(newBuffer);
-
-                        foreach (var topic in subscribeMsg.Topics)
-                        {
-                            if (_clientSubscriptions[client.ClientId].Contains(topic))
-                                continue;//Already subscribed, ignore the request
-
-                            _logger.Log(LogLevel.Trace, $"Adding subscription to topic '{topic}' for client '{client.ClientId}'");
-                            _clientSubscriptions[client.ClientId].Add(topic);
-                        }
-
-                        BeginSend(client, new Shared.Messages.SubscribeAck(subscribeMsg.PacketId).GetBytes());
-                        break;
-                    case MsgType.PublishAck:
-                        break;
-                    default:
-                        _logger.Log(LogLevel.Warn, $"Ignoring message: '{Encoding.UTF8.GetString(newBuffer)}'");
-                        break;
-                }
-
-                socket.BeginReceive(client.Buffer, 0, 128000, 0, ReceiveCallback, client);
-            }
-            finally 
-            {
-                _semaphoreSlim.Release();
-            }
-        }
-
-        private void BeginSend(ClientState clientState, byte[] buffer)
-        {
-            clientState.Socket.BeginSend(buffer, 0, buffer.Length, 0, SendCallback, clientState.Socket);
-        }
-
-        private void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                var socket = (Socket)ar.AsyncState;
-                var bytesSent = socket.EndSend(ar);
-
-                _logger.Log(LogLevel.Trace, $"Sent '{bytesSent}' bytes");
-            }
-            catch (Exception e)
-            {
-                _logger.Log(LogLevel.Error, e);
-            }
         }
     }
 }
