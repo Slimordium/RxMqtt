6642cc8cdceac31afe957c4cc2d239292e97f818
Larry Watkins <slimordium@gmail.com>
2019-06-21 22:45:41 -0600

    Rough attempt at a UWP. Going to use Xamarin.Forms instead

---
+++ RxMqttUI/App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Caliburn.Micro;
+using RxMqttUI.ViewModels;
+using RxMqttUI.Views;
+
+namespace RxMqttUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App
+    {
+        private WinRTContainer _container;
+
+        public App()
+        {
+            Initialize();
+            InitializeComponent();
+        }
+
+        protected override void Configure()
+        {
+            _container = new WinRTContainer();
+
+            _container.RegisterWinRTServices();
+
+            _container.Singleton<ShellViewModel>();
+
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            _container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            DisplayRootView<ShellView>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ RxMqttUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Caliburn.Micro;
+
+namespace RxMqttUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+
+        public IObservableCollection<PivotItem> PivotViewItems { get; set; } = new BindableCollection<PivotItem>();
+
+        public ShellViewModel()
+        {
+            PivotViewItems.Add(new PivotItem { Content = "Broker test" });
+            PivotViewItems.Add(new PivotItem { Content = "Client test" });
+            NotifyOfPropertyChange(nameof(PivotViewItems));
+
+        }
+
+        public async Task Back()
+        {
+            PivotViewItems = new BindableCollection<PivotItem>();
+            PivotViewItems.Add(new PivotItem { Content = "Client test" });
+            NotifyOfPropertyChange(nameof(PivotViewItems));
+        }
+
+        public async Task Forward()
+        {
+            PivotViewItems = new BindableCollection<PivotItem>();
+            PivotViewItems.Add(new PivotItem { Content = "Broker test" });
+            NotifyOfPropertyChange(nameof(PivotViewItems));
+        }
+    }
+}
